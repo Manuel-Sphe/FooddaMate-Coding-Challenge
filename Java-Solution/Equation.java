b6535d1d9b4bc248b957d084b94560aaa0ccbf1f
Sphesihle Madonsela <49991613+Manuel-Sphe@users.noreply.github.com>
2022-09-12 23:14:19 +0200

    simple case solved

--- Java-Solution/Equation.java
+++ Java-Solution/Equation.java
@@ -20,25 +20,48 @@ public class Equation {
         return ""+sign;
     }
 
+    public String getNum(){
+        return this.left.trim().substring(left.indexOf(getSign())+1);
+    }
+
     public void  Eliminate(){
         String result = "";
-        String num = this.left.trim().substring(left.indexOf(getSign())+2);
+        String num = getNum();
        
        
         if(getSign().equals("-")){
             System.out.println("Add "+num+" to both sides");
-            result  = this.left + "+ " + num + " =" +right+" + "+num;
+            result  = this.left + " + " + num + " =" +right+" + "+num;
         }
         else if(getSign().equals("+")){
             System.out.println("Substract "+num+" to both sides");
-            result = this.left +"- "+num + " ="+ right+" - "+num;
+            result = this.left +" - "+num + " = "+ right+" - "+num;
         }
 
         System.out.println(result);
     }
 
-    public void Simpify(String l, String r){
+    public void Simpify(){
+        System.out.println("Simplify");
+        int rhs = Integer.parseInt(right.trim());
+        int ans = rhs + Integer.parseInt(getNum());
 
+        String lhs = left.substring(0,left.indexOf('x')+1);
+        
+        System.out.println(lhs +" = "+ ans);
+
+        setLeft(lhs);
+        setRight(""+ans);
+
+    }
+
+    void Final_Ans(){
+        
+        String l = getLeft();
+        
+        String x_coff = l.substring(0, l.indexOf('x'));
+        System.out.println("Devide both side by the same factor");
+        System.out.println("x = "+getRight()+'/'+x_coff);
     }
 
     void setLeft( String l){
@@ -46,6 +69,14 @@ public class Equation {
     }
     void setRight(String r){
         this.right = r;
+    }
+
+    String getLeft(){
+        return this.left;
+    }
+
+    String getRight(){
+        return this.right;
     }
 
 
