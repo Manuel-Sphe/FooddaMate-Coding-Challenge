b894abd20d1e046f39bf879f75156f1785c3653e
Sphesihle Madonsela <49991613+Manuel-Sphe@users.noreply.github.com>
2022-09-27 00:55:54 +0200

    More methods are tested

--- Unit_test.py
+++ Unit_test.py
@@ -1,19 +1,31 @@
 import unittest
 from Expession import Expression
 from Equation import Equation
+import Solution
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_expand(self):
+        ex = Expression('2(2x+3)+2(3x-3)',"40")
+        self.assertEqual(ex.Expand(["+2(2x+3)","+2(3x-3)"]), ['+4x+6','+6x-6'])
 
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
         self.assertFalse('Foo'.isupper())
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
+    def test_parethesis(self):
+        ex = Expression('7x-5','2')
+        self.assertTrue(ex.hasParethesis('2(2-x)'))
+        self.assertFalse(ex.hasParethesis(ex.getLeft()))
+        self.assertEqual(ex.getLeft(),'7x-5')
+
+
+    def test_solve(self):
+        s = '7x-5=2'
+        sp = Solution.Solve(s)
+        self.assertEqual(sp, 'x=1.00')
+        self.assertEqual(s.split('='),['7x-5','2'])
+
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
