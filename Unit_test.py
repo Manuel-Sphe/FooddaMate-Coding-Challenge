614483b5ba7d144244925e0c0ee9ac884b751539
Sphesihle Madonsela <49991613+Manuel-Sphe@users.noreply.github.com>
2022-10-02 01:03:38 +0200

    Updated units on the expressions method

--- Unit_test.py
+++ Unit_test.py
@@ -5,13 +5,29 @@ import Solution
 
 class TestStringMethods(unittest.TestCase):
 
+    def test_modify(self):
+        ex  = Expression('','')
+        self.assertEqual(ex.modify(['+x','3x']),['+1x','3x'])
+        self.assertEqual(ex.modify(['-x','6x','+x']),['-1x','6x','+1x'])
+
+    def test_modifyConst(self):
+        ex = Expression('','')
+        self.assertEqual(ex.modify_const('4+x'),'+4+x')
+        
     def test_expand(self):
         ex = Expression('2(2x+3)+2(3x-3)',"40")
         self.assertEqual(ex.Expand(["+2(2x+3)","+2(3x-3)"]), ['+4x+6','+6x-6'])
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_getSign(self):
+        ex = Expression('2x+3','3x-1')
+        self.assertTrue(ex.getSign(ex.getLeft())==2)
+
+        
+    def test_hasParentessis(self):
+        ex = Expression('2(x+3)','4x+3')
+        self.assertTrue(ex.hasParethesis(ex.getLeft()))
+        self.assertFalse(ex.hasParethesis(ex.getRight()))
+
 
     def test_parethesis(self):
         ex = Expression('7x-5','2')
